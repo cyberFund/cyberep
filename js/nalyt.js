cf18968b1115f67dbb48c36834d1ba08e32aeb8d
Konstantin Lomashuk <lomashuk@gmail.com>
2015-08-05 13:22:28 +0200

    Change name of the event

--- js/nalyt.js
+++ js/nalyt.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   if (analytics) {
     $('a[target=_blank]').click(function (e) {
       var url = this.getAttribute("href") ||  this.getAttribute("xlink:href");
-      analytics.track("externalLink", {url: url});
+      analytics.track("Clicked External Link", {url: url});
     });
 
     window.onpopstate = function (event) {
