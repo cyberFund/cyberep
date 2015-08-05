182173d7e8ead8171d67d77283baba6c3229c779
Konstantin Lomashuk <lomashuk@gmail.com>
2015-08-05 14:38:16 +0200

    Test event

--- js/nalyt.js
+++ js/nalyt.js
@@ -5,6 +5,11 @@ $(document).ready(function(){
       analytics.track("Clicked External Link", {url: url});
     });
 
+    $('a.share-reveal-facebook').click(function (e) {
+         var url = this.getAttribute("href") ||  this.getAttribute("xlink:href");
+         analytics.track("Clicked Social", {url: url});
+       });
+
     window.onpopstate = function (event) {
       analytics.page("Slide", {url: location.href});
     }
