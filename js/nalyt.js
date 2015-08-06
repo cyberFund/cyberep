5f8bc7947208faf9fcaa3ef807783d0df6931351
Konstantin Lomashuk <lomashuk@gmail.com>
2015-08-06 14:17:10 +0200

    Add Social Events

--- js/nalyt.js
+++ js/nalyt.js
@@ -2,12 +2,17 @@ $(document).ready(function(){
   if (analytics) {
     $('a[target=_blank]').click(function (e) {
       var url = this.getAttribute("href") ||  this.getAttribute("xlink:href");
-      analytics.track("Clicked External Link", {url: url});
+      analytics.track("Clicked Cyberep External Link", {url: url});
     });
 
     $('a.share-reveal-facebook').click(function (e) {
          var url = this.getAttribute("href") ||  this.getAttribute("xlink:href");
-         analytics.track("Clicked Social", {url: url});
+         analytics.track("Clicked Cyberep Social", {url: url});
+       });
+       
+    $('a.twitter-share-button').click(function (e) {
+         var url = this.getAttribute("href") ||  this.getAttribute("xlink:href");
+         analytics.track("Clicked Cyberep Social", {url: url});
        });
 
     window.onpopstate = function (event) {
