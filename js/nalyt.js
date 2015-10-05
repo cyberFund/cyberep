2fc8f8cc18b55a6faace11d588bd169f02dc5c06
Konstantin Lomashuk <lomashuk@gmail.com>
2015-10-05 23:38:32 +0200

    Changed Events Names
    Clicked Cyberep External Link => Clicked External Link
    Clicked Cyberep Social => Shared a Thing

--- js/nalyt.js
+++ js/nalyt.js
@@ -2,17 +2,17 @@ $(document).ready(function(){
   if (analytics) {
     $('a[target=_blank]').click(function (e) {
       var url = this.getAttribute("href") ||  this.getAttribute("xlink:href");
-      analytics.track("Clicked Cyberep External Link", {url: url});
+      analytics.track("Clicked External Link", {url: url});
     });
 
     $('a.share-reveal-facebook').click(function (e) {
          var url = this.getAttribute("href") ||  this.getAttribute("xlink:href");
-         analytics.track("Clicked Cyberep Social", {url: url});
+         analytics.track("Shared a Thing", {url: url});
        });
-       
+
     $('a.twitter-share-button').click(function (e) {
          var url = this.getAttribute("href") ||  this.getAttribute("xlink:href");
-         analytics.track("Clicked Cyberep Social", {url: url});
+         analytics.track("Shared a Thing", {url: url});
        });
 
     window.onpopstate = function (event) {
